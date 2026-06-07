9d037eb0b684333ab37827bcfba694ffcdc9e190
Hai <bomnguyen879@gmail.com>
2026-06-07 16:47:08 +0700

    Hoàn thành kiểm chứng câu A1

---
+++ PBT_07/var_let_const.js
@@ -0,0 +1,35 @@
+console.log("--- Đoạn 1 ---");
+console.log(x); 
+var x = 5;
+
+console.log("--- Đoạn 2 ---");
+try {
+    console.log(y); 
+    let y = 10;
+} catch (error) {
+    console.log("Lỗi Đoạn 2:", error.message);
+}
+
+console.log("--- Đoạn 3 ---");
+try {
+    const z = 15;
+    z = 20; 
+    console.log(z);
+} catch (error) {
+    console.log("Lỗi Đoạn 3:", error.message);
+}
+
+
+console.log("--- Đoạn 4 ---");
+const arr = [1, 2, 3];
+arr.push(4); 
+console.log(arr); 
+
+
+console.log("--- Đoạn 5 ---");
+let a = 1;
+{
+    let a = 2;
+    console.log("Trong block:", a); 
+}
+console.log("Ngoài block:", a); 
